62211ba54c90347f7fd25d169c10769537da82fe
Vikas <81951052+vkas9@users.noreply.github.com>
2023-10-11 19:16:59 +0000

    m

--- landing /script.js
+++ landing /script.js
@@ -1,1 +1,23 @@
-Shery.imageEffect(".back",{style:2,debug:true});
+Shery.imageEffect(".back",{style:2,debug:true,goeey:true});
+
+
+var h1s=document.querySelectorAll("h1");
+var index=0;
+document.querySelector(".main").addEventListener("click",()=>{
+    gsap.to(h1s[index],{
+        top:"-=100%",
+        ease:Expo.easeInOut,
+        duration:1,
+        onComplete:function(){
+            gsap.set(this.targets[0],{top:"100%"});
+        },
+    });
+    
+    index===h1s.length-1?(index=0):index++;
+    gsap.to(h1s[index],{
+        top:"-=100%",
+        ease:Expo.easeInOut,
+        duration:1,
+    });
+    
+})
